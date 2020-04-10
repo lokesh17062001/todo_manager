a0e1b43993e0c3f508959bf9ef85fded0fdb2cdd
Lokesh17062001 <122003169@satra.ac.in>
2020-04-10 12:06:55 +0530

    added an method to create a new todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,4 +8,16 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     render plain: todo.to_pleasant_string
   end
+
+  def create
+    todo_text = params[:todo_text]
+    date_submission = DateTime.parse(params[:date_submission])
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      date_submission: date_submission,
+      completed: false,
+    )
+    response_text = "created new todo with id #{new_todo.id}"
+    render plain: response_text
+  end
 end
