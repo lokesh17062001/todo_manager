d42699804e937073139123adc7d59557850701ad
Lokesh17062001 <122003169@satra.ac.in>
2020-05-03 10:52:03 +0530

    database verified

--- db/migrate/20200503051302_create_users.rb
+++ db/migrate/20200503051302_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.text :name
+      t.string :name
       t.string :email
-      t.string :password
+      t.string :pasword
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_094502) do
+ActiveRecord::Schema.define(version: 2020_05_03_051302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 2020_04_19_094502) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.text "name"
+    t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "pasword"
   end
+
 end
